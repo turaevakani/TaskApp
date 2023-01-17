27ad6bc62ac799453858dbb86d6e0a59d5b4c6d2
turaeva kani <kanykeituraeva03@gmail.com>
2023-01-17 18:30:02 +0600

    change icon

--- app/src/main/java/com/geektech/taskapp/MainActivity.kt
+++ app/src/main/java/com/geektech/taskapp/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.taskFragment
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile,  R.id.taskFragment
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
